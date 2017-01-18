d2851a8b401401a6a651ba230b1598f8b7499a23
kang <xiyoulinux.kangyijie@gmail.com>
2017-01-18 16:35:32 +0800

    update

---
+++ Winter/3-14.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: 3-14.cpp
+	> Author: Jack Kang
+	> Mail: kangyijie@xiyoulinux.org
+	> Created Time: 2017年01月15日 星期日 10时32分53秒
+ ************************************************************************/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(void)
+{
+    vector<int> a;
+    int temp;
+    while(std::cin >> temp){
+        a.push_back(temp);
+    }
+    //std::cout << a << std::endl;
+}
---
+++ Winter/3-15.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: 3-15.cpp
+	> Author: Jack Kang
+	> Mail: kangyijie@xiyoulinux.org
+	> Created Time: 2017年01月15日 星期日 10时41分12秒
+ ************************************************************************/
+
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+int main(void)
+{
+    vector<string> a;
+    string b;
+    while(std::cin >> b){
+        a.push_back(b);
+    }
+}
