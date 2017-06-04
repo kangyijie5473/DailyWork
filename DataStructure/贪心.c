cd4c409d9b76ca0d0e845c356c71ce02fab0fe8b
kang <xiyoulinux.kangyijie@gmail.com>
2017-06-04 22:30:35 +0800

    小小尝试贪心

---
+++ DataStructure/贪心.c
@@ -0,0 +1,47 @@
+/*
+ * > File Name: 贪心.c
+ * > Author: Jack Kang
+ * > Mail: kangyijie@xiyoulinux.org
+ * > Created Time: 2017年06月04日 星期日 11时59分13秒
+ */
+
+#include <stdio.h>
+struct item{
+    int begin;
+    int end;
+    int value;
+};
+struct item it[100];
+int max(int a,int b){
+    if(a > b)
+        return a;
+    else return b;
+}
+int result(int n)
+{
+    int greedy[n];
+    //按照结束时间排序 此处省略200个字
+    greedy[0] = it[0].value; // 第一次的值直接得到
+    for(int i = 1; i < n; ++i){
+        int j;
+        for(j = i -1; j >=0;--j){
+            if(it[j].end < it[i].begin)
+                break;
+        }
+        if(j == -1)
+            greedy[i] = max(it[i].value,it[i-1].value);
+        else 
+            greedy[i] = it[j].value + it[i].value;
+    }
+    return greedy[n-1];
+}
+int main(void)
+{
+    int n;
+    scanf("%d",&n);
+    for(int i = 0; i < n; i++){
+        scanf("%d%d%d",&it[i].begin,&it[i].end,&it[i].value);
+    }
+    printf("result : %d\n",result(n));
+
+}
