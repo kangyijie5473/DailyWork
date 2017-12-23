9f0239896d723eca48309071cadb5a0fff022b21
kangkang <xiyoulinux.kangyijie@gmail.com>
2017-12-23 16:52:06 +0800

    C++11 raw 字符串

---
+++ Exercise/cpp/R.cpp
@@ -0,0 +1,15 @@
+/*
+ * @filename:    R.cpp
+ * @author:      Crow
+ * @date:        2017年12月10日 11:22:43
+ * @description:
+ */
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main(void)
+{
+    string a = R"(hello\\\\///)";
+    cout <<  a << endl;
+}
